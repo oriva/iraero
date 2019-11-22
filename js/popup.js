0a235171314bd739168e94d8711138e74d5d7e6f
Oriva <weelman93@gmail.com>
2019-11-22 16:24:57 +0800

    save

--- js/popup.js
+++ js/popup.js
@@ -241,16 +241,16 @@ const popupWindows = (() => {
     const createGallery = () => {
         disabledEvents = true;
         tourBlock.querySelector('.tour-3d__content').innerHTML = '<div class="owl-carousel owl-gallery">\n' +
-            '  <div><img src="img/popup/gallery/10.jpg" alt=""></div>\n' +
-            '  <div><img src="img/popup/gallery/2.jpg" alt=""></div>\n' +
             '  <div><img src="img/popup/gallery/1.jpg" alt=""></div>\n' +
+            '  <div><img src="img/popup/gallery/2.jpg" alt=""></div>\n' +
             '  <div><img src="img/popup/gallery/3.jpg" alt=""></div>\n' +
+            '  <div><img src="img/popup/gallery/4.jpg" alt=""></div>\n' +
             '  <div><img src="img/popup/gallery/5.jpg" alt=""></div>\n' +
             '  <div><img src="img/popup/gallery/6.jpg" alt=""></div>\n' +
             '  <div><img src="img/popup/gallery/7.jpg" alt=""></div>\n' +
+            '  <div><img src="img/popup/gallery/8.jpg" alt=""></div>\n' +
             '  <div><img src="img/popup/gallery/9.jpg" alt=""></div>\n' +
-            '  <div><img src="img/popup/gallery/11.jpg" alt=""></div>\n' +
-            '  <div><img src="img/popup/gallery/12.jpg" alt=""></div>\n' +
+            '  <div><img src="img/popup/gallery/10.jpg" alt=""></div>\n' +
             '</div>';
         setTimeout(()=>{
             $(".owl-gallery").owlCarousel({
@@ -360,7 +360,7 @@ const popupWindows = (() => {
 const popupDots = (() => {
     const dotsContainer = document.querySelector('.dots__container');
     // getPosition left,top(%), dataId
-    const getPosition = [[[41, 43, 1], [72, 43, 2], [44, 71, 3]], [[57, 49, 4], [72, 58, 5], [88, 48, 6]], [[48, 22, 7], [53, 53, 8], [82, 18, 9]], [[70, 35, 15], [94, 48, 14], [26, 24, 13]], [[51, 69, 10], [67, 44, 11], [35, 44, 12]]];
+    const getPosition = [[[41, 43, 1], [172, 143, 2], [44, 71, 3]], [[57, 49, 4], [72, 58, 5], [88, 48, 6]], [[48, 22, 7], [53, 53, 8], [82, 18, 9]], [[70, 35, 15], [94, 48, 14], [26, 24, 13]], [[51, 69, 10], [67, 44, 11], [35, 44, 12]]];
     const dotElemCreate = leftTop => {
         let duru = document.createElement('div');
         duru.className = 'dots__item-container hide';
